e86cfc6a34f80d5f8d67d946116161df7ad8d396
fcollado91 <fcollado@datamart.co>
2024-10-29 17:47:56 -0300

    Add content for components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-50 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
